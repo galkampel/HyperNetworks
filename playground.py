f648ce352bf9cef02fcf598551aafe57f9e934a4
galkampel <galkampel@mail.tau.ac.il>
2020-08-13 11:30:20 +0300

    buiding training set from Data and copy GATConv example (playground.py)

--- playground.py
+++ playground.py
@@ -1,48 +1,103 @@
 
 import os
 import torch
+import torch.nn.functional as F
+from torch_geometric.datasets import Planetoid
+import torch_geometric.transforms as T
+from torch_geometric.nn import GATConv
 
-# from torch_geometric.nn import SchNet
-from model.gan import GATConv
-from utils.preprocess import TransductivePreprocessing
-from torch_geometric.data import DataLoader
-from torch_geometric.datasets import CitationFull
 
-names = {"Cora", "Cora_ML", "CiteSeer", "PubMed"}
-name = "PubMed"
+names = {"Cora", "CiteSeer", "PubMed"}
+name = "Cora"
 path = os.path.join('/home/galkampel/tmp', name)
-dataset = CitationFull(path, name)
-t_preprocess = TransductivePreprocessing(nodes_per_class=20, n_validation=500, n_test=1000)
-train_idx, val_idx, test_idx = t_preprocess.train_val_test_idx_split(dataset.data.y)
-train_set, validation_set, test_set = dataset[train_idx], dataset[val_idx], dataset[test_idx]
-train_loader = DataLoader(train_set, batch_size=1, shuffle=True)  # 1?
-val_loader = DataLoader(validation_set, batch_size=1)  # 1?
-test_loader = DataLoader(test_set, batch_size=1)  # 1?
-for data_batch in train_loader:
-    x = 5
-    # data_batch = data_batch.to(device)
-# device = torch.device('cuda:3' if torch.cuda.is_available() else 'cpu')
-exit()
+dataset = Planetoid(path, name, transform=T.NormalizeFeatures())
+data = dataset[0]
 
-# for target in range(12):
-#     model, datasets = SchNet.from_qm9_pretrained(path, dataset, target)
-#     train_dataset, val_dataset, test_dataset = datasets
 
-    # model = model.to(device)
-    # loader = DataLoader(test_dataset, batch_size=256)
+class GAT(torch.nn.Module):
+    def __init__(self):
+        super(GAT, self).__init__()
+        self.conv1 = GATConv(dataset.num_features, 8, heads=8, dropout=0.6)
+        # On the Pubmed dataset, use heads=8 in conv2.
+        self.conv2 = GATConv(8 * 8, dataset.num_classes, heads=1, concat=False,
+                             dropout=0.6)
 
-    # maes = []
-    # for data in loader:
-    #     data = data.to(device)
-        # with torch.no_grad():
-            # pred = model(data.z, data.pos, data.batch)
-        # mae = (pred.view(-1) - data.y[:, target]).abs()
-        # maes.append(mae)
+    def forward(self):
+        x = F.dropout(data.x, p=0.6, training=self.training)
+        x = F.elu(self.conv1(x, data.edge_index))
+        x = F.dropout(x, p=0.6, training=self.training)
+        x = self.conv2(x, data.edge_index)
+        return F.log_softmax(x, dim=1)
 
-    # mae = torch.cat(maes, dim=0)
 
-    # Report meV instead of eV.
-    # mae = 1000 * mae if target in [2, 3, 4, 6, 7, 8, 9, 10] else mae
+device = torch.device('cuda:3' if torch.cuda.is_available() else 'cpu')
+model, data = GAT().to(device), data.to(device)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.005, weight_decay=5e-4)
 
-    # print(f'Target: {target:02d}, MAE: {mae.mean():.5f} ± {mae.std():.5f}')
 
+def train():
+    model.train()
+    optimizer.zero_grad()
+    F.nll_loss(model()[data.train_mask], data.y[data.train_mask]).backward()
+    optimizer.step()
+
+
+def test():
+    accs = []
+    with torch.no_grad():
+        model.eval()
+        logits = model()
+        for _, mask in data('train_mask', 'val_mask', 'test_mask'):
+            pred = logits[mask].max(1)[1]
+            acc = pred.eq(data.y[mask]).sum().item() / mask.sum().item()
+            accs.append(acc)
+    return accs
+
+
+for epoch in range(1, 301):
+    train()
+    log = 'Epoch: {:03d}, Train: {:.4f}, Val: {:.4f}, Test: {:.4f}'
+    print(log.format(epoch, *test()))
+
+# import os
+# import torch
+# from torch.utils.data import Subset
+# # from torch_geometric.nn import SchNet
+# from model.gan import GATConv
+# from utils.preprocess import TransductivePreprocessing
+# from torch_geometric.data import DataLoader, Data, Dataset
+# import torch_geometric.transforms as T
+# from torch_geometric.datasets import Planetoid  # QM9, CitationFull
+#
+# names = {"Cora", "CiteSeer", "PubMed"}
+# name = "PubMed"
+# path = os.path.join('/home/galkampel/tmp', name)
+# train_batch_size = 1
+# test_batch_size = 1
+# dataset = Planetoid(path, name, transform=T.NormalizeFeatures())
+# data = dataset[0]
+# x = 3
+# # data = dataset[0]
+# # path2 = os.path.join('/home/galkampel/tmp', "QM9")
+# # dataset2 = QM9(root=path2)
+# t_preprocess = TransductivePreprocessing(nodes_per_class=20, n_validation=500, n_test=1000)
+# train_idx, validation_idx, test_idx = t_preprocess.train_val_test_idx_split(dataset.data.y)
+# # train_idx = torch.from_numpy(train_idx).long()
+# # validation_idx, test_idx = torch.from_numpy(validation_idx).long(), torch.from_numpy(test_idx).long()
+# # train_idx, val_idx, test_idx = t_preprocess.train_val_test_idx_split(dataset.data.y)
+#
+# # data_train = Data(x=data.x[train_mask], y=data.y[train_mask], edge_index=data.edge_index)
+# training_set = t_preprocess.create_dataset(data, train_idx, data_size=1)
+# train_loader = DataLoader(training_set, batch_size=16, shuffle=True)
+#
+# # validation_set = t_preprocess.create_dataset(data, validation_idx, data_size=test_batch_size)
+# # train_loader = DataLoader(validation_set, batch_size=16, shuffle=True)
+#
+# # training_set = t_preprocess.create_dataset(data, train_idx, data_size=train_batch_size)
+# # train_loader = DataLoader(training_set, batch_size=train_batch_size, shuffle=True)
+#
+# count = 1
+# for data_batch in train_loader:
+#     print(f'count = {count}')
+#     count += 1
+# x = 5
--- utils/preprocess.py
+++ utils/preprocess.py
@@ -1,6 +1,7 @@
 
 import numpy as np
-
+import torch
+from torch_geometric.data import Data
 
 class TransductivePreprocessing:
     def __init__(self, nodes_per_class=20, n_validation=500, n_test=1000, seed=0):
@@ -23,9 +24,16 @@ class TransductivePreprocessing:
         labels_split[np.random.choice(labels)] += resid
         return labels_split
 
+    @staticmethod
+    def set_mask(N, idx):
+        mask = np.zeros(N, dtype=np.bool)
+        mask[idx] = True  # np.True_
+        return mask
+
     def train_val_test_idx_split(self, y):
         labels = y.unique().numpy()
-        idx_arr = np.arange(len(y))
+        N = len(y)
+        idx_arr = np.arange(N)
         train_idx, val_idx, test_idx = [], [], []
         val_label_split = self.set_size_per_label(self.n_validation, labels)
         test_label_split = self.set_size_per_label(self.n_test, labels)
@@ -39,7 +47,30 @@ class TransductivePreprocessing:
             rel_idx = np.setxor1d(rel_idx, val_label_idx)
             test_label_idx = self.create_label_idx(rel_idx, test_label_split[label])
             test_idx += test_label_idx.tolist()
+
         return train_idx, val_idx, test_idx
+        # train_mask = self.set_mask(N, train_idx)
+        # val_mask = self.set_mask(N, val_idx)
+        # test_mask = self.set_mask(N, test_idx)
+        # return train_mask, val_mask, test_mask
+
+    def create_dataset(self, data, idx, data_size):
+        data_list = []
+        cur_idx = idx.copy()
+        N = len(cur_idx)
+        while N >= data_size:
+            rel_idx = np.random.choice(cur_idx, data_size, replace=False)
+            data_idx = torch.from_numpy(rel_idx).long()
+            cur_data = Data(x=data.x[data_idx], y=data.y[data_idx], edge_index=data.edge_index)
+            data_list.append(cur_data)
+            cur_idx = np.setxor1d(cur_idx, rel_idx)
+            N = len(cur_idx)
+
+        if N > 0:
+            data_idx = torch.from_numpy(cur_idx).long()
+            cur_data = Data(x=data.x[data_idx], y=data.y[data_idx], edge_index=data.edge_index)
+            data_list.append(cur_data)
+        return data_list
 
 
 
